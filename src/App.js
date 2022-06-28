87e8fd52d3be590bb5c487db8d870ad018e378c0
LamkadamNabil <na.lamkadam@gmailcom>
2022-06-28 18:01:57 +0100

    My first react ecosystem

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react'
+const  App =()=> (
+ 
+    <div className="App">
+      <h1>
+          hello world
+      </h1>
+    </div>
+
+)
+
+export default App;
